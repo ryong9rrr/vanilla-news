638816c6ed2ca08286e178a20085c77c7b55efa4
ryong9rrr <tkddbs1644@gmail.com>
2022-01-24 23:15:38 +0900

    feat: newsFeed와 newsDetail 렌더링, 화면전환

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
-const root = document.getElementById("root");
+const $ = (selector) => document.querySelector(selector);
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = `https://api.hnpwa.com/v0/item/@id.json`;
 const ajax = new XMLHttpRequest();
-const NEWS_API = "https://api.hnpwa.com/v0/news/1.json";
+let unit = 10;
+let page = 1;
 
-ajax.open("GET", NEWS_API, false);
-ajax.send();
-
-const newsFeed = JSON.parse(ajax.response);
-const newsList = [];
-newsList.push("<ul>");
-for (let i = 0; i < newsFeed.length; i++) {
-  newsList.push(`<li>${newsFeed[i].title}</li>`);
+function getData(url) {
+  ajax.open("GET", url, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
 }
-newsList.push("</ul>");
 
-//root.innerHTML = newsList.join("");
+const newsFeed = getData(NEWS_URL);
+
+const render = (view) => {
+  $("#root").innerHTML = view();
+};
+
+const newFeedView = () => {
+  const liList = [];
+  for (let i = 0; i < unit; i++) {
+    liList.push(
+      `<li><a href="#${newsFeed[i].id}">${newsFeed[i].title}(좋아요:${newsFeed[i].points} / 댓글:${newsFeed[i].comments_count})</a></li>`
+    );
+  }
+  return `<ul>${liList.join("")}</ul>`;
+};
+
+$("#select").addEventListener("change", (e) => {
+  console.log(e.target.id);
+  const selectedUnit = Number(
+    $("#select").options[$("#select").selectedIndex].value
+  );
+  unit = selectedUnit;
+  render(newFeedView);
+});
+
+window.addEventListener("hashchange", (e) => {
+  const id = location.hash.slice(1);
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
+
+  const template = () => `
+    <div>
+      <span><a href=/>홈으로</a></span>    
+      <section>
+        <h1>${newsContent.title}</h1>
+        <div>좋아요 : ${newsContent.points}</div>
+        <div>댓글 수 : ${newsContent.comments_count}</div>
+      </section>
+    </div>
+  `;
+
+  render(template);
+});
+
+render(newFeedView);
