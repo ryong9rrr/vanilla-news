769c6b9f155b631d09ee0d78b4a8055f6bc9623c
ryong9rrr <tkddbs1644@gmail.com>
2022-01-25 23:10:16 +0900

    refactor: 문자열 템플릿형태로 리팩터링

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const $ = (selector) => document.querySelector(selector);
-const NEWS_URL = "https://api.hnpwa.com/v0/news/@pagination.json";
+const root = document.getElementById("root");
+const NEWS_URL = "https://api.hnpwa.com/v0/news/@paging.json";
 const CONTENT_URL = `https://api.hnpwa.com/v0/item/@id.json`;
 const store = {
   currentPage: 1,
@@ -12,54 +12,93 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const pageListTemplate = () => {
-  const pageList = [`<nav><ul class="nav">`];
-  const start = Math.floor((store.currentPage - 1) / 10) * 10 + 1;
-  for (let i = start; i < start + 10; i++) {
-    if (i === store.currentPage) {
-      pageList.push(`<li class="current"><a href="#/page/${i}">${i}</a></li>`);
-    } else {
-      pageList.push(`<li><a href="#/page/${i}">${i}</a></li>`);
-    }
-  }
-  pageList.push(`</ul></nav>`);
-
-  const template = pageList.join("");
-
-  return template;
-};
-
 function newsFeed() {
-  const pagination = Math.floor((store.currentPage - 1) / 3) + 1;
-  const newsFeedData = getData(NEWS_URL.replace("@pagination", pagination));
-  const newsList = [];
-  const table = [20, 0, 10];
-  const start = table[store.currentPage % 3];
-  for (let i = start; i < start + 10; i++) {
-    newsList.push(`
+  const paging = Math.floor((store.currentPage - 1) / 3) + 1;
+  const newsFeedData = getData(NEWS_URL.replace("@paging", paging));
+
+  const makePagination = () => {
+    const makePageNumbers = () => {
+      const pageList = [];
+      const start = Math.floor((store.currentPage - 1) / 10) * 10 + 1;
+      for (let i = start; i < start + 10; i++) {
+        if (i === store.currentPage) {
+          pageList.push(
+            `<li><a href="#/page/${i}"><strong>${i}</strong></a></li>`
+          );
+        } else {
+          pageList.push(`<li><a href="#/page/${i}">${i}</a></li>`);
+        }
+      }
+      return pageList.join("");
+    };
+
+    let template = `
+      <nav>
+        <a href="#">처음</a>
+        <a href="#/page/{{__prev_page__}}">이전</a>
+        <ul>
+          {{_pagination_}}
+        </ul>
+        <a href="#/page/{{__next_page__}}">다음</a>
+        <a href="#/page/30">마지막</a>
+      </nav>
+    `;
+
+    let updatedTemplate = template;
+
+    updatedTemplate = updatedTemplate.replace(
+      "{{_pagination_}}",
+      makePageNumbers()
+    );
+
+    updatedTemplate = updatedTemplate.replace(
+      "{{__prev_page__}}",
+      store.currentPage === 1 ? 1 : store.currentPage - 1
+    );
+
+    updatedTemplate = updatedTemplate.replace(
+      "{{__next_page__}}",
+      store.currentPage === 30 ? 30 : store.currentPage + 1
+    );
+
+    return updatedTemplate;
+  };
+
+  const makeFeed = () => {
+    const newsList = [];
+    const table = [20, 0, 10];
+    const start = table[store.currentPage % 3];
+    for (let i = start; i < start + 10; i++) {
+      newsList.push(`
         <li>
           <a href="#/show/${newsFeedData[i].id}">
             ${newsFeedData[i].title}(좋아요:${newsFeedData[i].points} / 댓글:${newsFeedData[i].comments_count})
           </a>
         </li>`);
-  }
+    }
+    return newsList.join("");
+  };
 
-  const template = `
-    <h1><a href="#">홈으로</a></h1>
-    <ul>${newsList.join("")}</ul>
+  let template = `
     <div>
-      <a href="#">⏪</a>
-      <a href="#/page/${
-        store.currentPage === 1 ? 1 : store.currentPage - 1
-      }">◀</a>
-      ${pageListTemplate()}
-      <a href="#/page/${
-        store.currentPage === 30 ? 30 : store.currentPage + 1
-      }">▶</a>
-      <a href="#/page/30">⏩</a>
+      <h1>해커 뉴스</h1>
+      <ul>
+        {{_news_feed_}}
+      </ul>
+      {{_pagination_}}
     </div>
   `;
-  return ($("#root").innerHTML = template);
+
+  let updatedTemplate = template;
+
+  updatedTemplate = updatedTemplate.replace("{{_news_feed_}}", makeFeed());
+
+  updatedTemplate = updatedTemplate.replace(
+    "{{_pagination_}}",
+    makePagination()
+  );
+
+  return (root.innerHTML = updatedTemplate);
 }
 
 function newsDetail() {
@@ -77,7 +116,7 @@ function newsDetail() {
     </div>
   `;
 
-  return ($("#root").innerHTML = template);
+  return (root.innerHTML = template);
 }
 
 function router() {
